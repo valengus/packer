07674bf1a2592cb54e692c8669c334315219ac25
Valentin Gusachenko <vgusachenko@gmail.com>
2021-12-20 12:55:01 +0200

    commit

--- scripts/InstallOpenSSHServer.ps1
+++ scripts/InstallOpenSSHServer.ps1
@@ -2,7 +2,7 @@
 Add-WindowsCapability -Online -Name (Get-WindowsCapability -Online | Where-Object Name -like 'OpenSSH.Server*').Name
 
 # set powershell as default shell
-New-ItemProperty -Path "HKLM:\SOFTWARE\OpenSSH" -Name DefaultShell -Value "C:\Windows\System32\WindowsPowerShell\v1.0\powershell.exe" -PropertyType String -Force
+# New-ItemProperty -Path "HKLM:\SOFTWARE\OpenSSH" -Name DefaultShell -Value "C:\Windows\System32\WindowsPowerShell\v1.0\powershell.exe" -PropertyType String -Force
 
 # enable sshd
 # Set-Service -Name sshd -StartupType Automatic
